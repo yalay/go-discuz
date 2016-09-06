676e87026314d07875f4c601d16bbbccb1ff1b5c
yalay <ya_lay@163.com>
2016-09-06 22:44:30 +0800

    fix:发布时增加延迟减少mysql压力

--- discuz/article.go
+++ discuz/article.go
@@ -140,5 +140,5 @@ func (article *Article) publish(config *tools.SqlConfig) {
 	discuzSql.GenTags(article, tid)
 	discuzSql.InsertPost(article, pid, tid)
 	discuzSql.UpdateForum(article, tid)
-	time.Sleep(time.Second)
+	time.Sleep(100 * time.Millisecond)
 }
